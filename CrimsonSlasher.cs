eda77e8872d4977a9b768f5ea097051eb209f88d
Timothy.Emerson <36423441+ItsAnEruption@users.noreply.github.com>
2018-05-01 20:17:38 -0700

    Add files via upload

--- CrimsonSlasher.cs
+++ CrimsonSlasher.cs
@@ -1,17 +1,39 @@
+using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace CrimsonSlasher
+namespace CrimsonSlasher.Items
 {
-	class CrimsonSlasher : Mod
+	public class CrimsonSlasher : ModItem
 	{
-		public CrimsonSlasher()
+		public override void SetStaticDefaults()
 		{
-			Properties = new ModProperties()
-			{
-				Autoload = true,
-				AutoloadGores = true,
-				AutoloadSounds = true
-			};
+			DisplayName.SetDefault("Crimtane Slasher");
+			Tooltip.SetDefault("A slashing sword made out of crimtane and tissue. Yuck...");
+		}
+		public override void SetDefaults()
+		{
+			item.damage = 28;
+			item.melee = true;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 15;
+			item.useAnimation = 15;
+			item.useStyle = 1;
+			item.knockBack = 4.5f;
+			item.value = 5050;
+			item.rare = 2;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.CrimtaneBar, 6);
+            recipe.AddIngredient(ItemID.TissueSample, 6);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
 		}
 	}
 }
